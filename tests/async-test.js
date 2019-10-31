e5bdf1fad77a21bd40d79a336617534065ebe89b
Jaime Flores <jaimelopesflores@gmail.com>
2019-10-31 15:48:15 +0100

    add unit tests

---
+++ tests/async-test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const reducer = require('../src/index')
+
+const arr = [1, 2, 3, 4]
+const asynCreate = async (a, i) => ({ ...a, [i]: Math.pow(i, 2) })
+const asyncSum = async (a, i) => a + i
+const promiseCreate = (a, i) => new Promise(resolve => resolve({ ...a, [i]: Math.pow(i, 2) }))
+const promiseSum = (a, i) => new Promise(resolve => resolve(a + i))
+
+describe('async', function() {
+  it('should perform a async/await reduction of a simple sum', async () => {
+    const summed = await reducer(arr, asyncSum, 0);
+    assert.equal(summed, 10);
+  });
+
+  it('should perform a promise reduction of a simple sum', async () => {
+    const summed = await reducer(arr, promiseSum, 0);
+    assert.equal(summed, 10);
+  });
+
+  it('should perform a async/await reduction of an object creation', async () => {
+    const obj = await reducer(arr, asynCreate, 0);
+    assert.deepEqual(obj, { '1': 1, '2': 4, '3': 9, '4': 16 });
+  });
+
+  it('should perform a promise reduction of an object creation', async () => {
+    const obj = await reducer(arr, promiseCreate, 0);
+    assert.deepEqual(obj, { '1': 1, '2': 4, '3': 9, '4': 16 });
+  });
+});
+
---
+++ tests/sync-test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const reducer = require('../src/index')
+
+const arr = [1, 2, 3, 4]
+const create = (a, i) => ({ ...a, [i]: Math.pow(i, 2) })
+const sum = (a, i) => a + i
+
+describe('sync', function() {
+  it('should perform a sync reduction of a simple sum', () => {
+    const summed = reducer(arr, sum, 0);
+    assert.equal(summed, 10);
+  });
+
+  it('should perform a sync reduction of an object creation', () => {
+    const obj = reducer(arr, create, {});
+    assert.deepEqual(obj, { '1': 1, '2': 4, '3': 9, '4': 16 });
+  });
+});
+
